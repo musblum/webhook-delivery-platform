a481c7237f586159ad762f24e9305ae11d57df58
Salem <musblum@gmail.com>
2026-09-04 22:59:10 -0400

    test: add PostgreSQL integration testing

---
+++ src/test/java/com/musblum/webhookdelivery/WebhookDeliveryPlatformTests.java
@@ -0,0 +1,13 @@
+package com.musblum.webhookdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebhookDeliveryPlatformTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
